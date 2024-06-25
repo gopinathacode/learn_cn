99778ac0ca2d56a7158eb024f405644b09aa006c
Gopinath <135225656+gopinathacode@users.noreply.github.com>
2024-06-25 17:23:45 +0530

    bedrock setup

--- web/wp-config.php
+++ web/wp-config.php
@@ -6,4 +6,4 @@
  */
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 require_once dirname(__DIR__) . '/config/application.php';
-// require_once ABSPATH . 'wp-settings.php';
+require_once ABSPATH . 'wp-settings.php';
